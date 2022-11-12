5d7b083c187ce59f25d6cec813dc70860b2dbfcf
AshwinPohane <ashwinpohane@gmail.com>
2022-11-12 16:12:28 +0530

    This is config file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+json_file_path = os.path.join('model', 'json_label_med_dataset.json')
+
+model_file_path = os.path.join('model', 'pickle_med_dataset.pkl')
+
+csv_file_path = os.path.join('model', 'medical_insurance.csv')
+
+port_number = 1002
